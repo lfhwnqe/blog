2fa49fc110556cc27a192afbac18add681186635
lfhwnqe <lfhwnqea@gmail.com>
2024-12-22 08:12:05 +0800

    feat: initialize redux package with basic store implementation and demo

---
+++ redux/demo/createStore.js
@@ -0,0 +1,17 @@
+export function createStore(reducer, initialState) {
+  let state = initialState;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+  };
+
+  return { getState, dispatch, subscribe };
+}
